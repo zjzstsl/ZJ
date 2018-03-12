11a3a3b8e867578e99568c03878c1d64f13f8ac9
shiyunlai <shiyunlai@gmail.com>
2018-03-12 23:55:45 +0800

    清理无效import

--- tools-starter-persistence/src/main/java/org/tis/tools/starter/configurer/MultiDataSourceAutoConfiguration.java
+++ tools-starter-persistence/src/main/java/org/tis/tools/starter/configurer/MultiDataSourceAutoConfiguration.java
@@ -4,7 +4,6 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
