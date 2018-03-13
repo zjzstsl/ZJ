50049ef963df658145100b6e4f46343c8aad6554
shiyunlai <shiyunlai@gmail.com>
2018-03-13 15:43:16 +0800

    使用开源swagger
    https://github.com/SpringForAll/spring-boot-starter-swagger

--- tools-for-test/src/main/java/org/tis/toolsfortest/ToolsForTestApplication.java
+++ tools-for-test/src/main/java/org/tis/toolsfortest/ToolsForTestApplication.java
@@ -1,12 +1,21 @@
 package org.tis.toolsfortest;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 应用启动入口
+ * 注解 @EnableSwagger2Doc ，启用swagger功能
+ * 注解 @SpringBootApplication 启动spring boot 应用
+ *
+ * @author Shiyunlai
+ */
+@EnableSwagger2Doc
 @SpringBootApplication
 public class ToolsForTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToolsForTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsForTestApplication.class, args);
+    }
 }
--- tools-for-test/src/main/java/org/tis/toolsfortest/usestarter/persistence/demo/controller/DemoController.java
+++ tools-for-test/src/main/java/org/tis/toolsfortest/usestarter/persistence/demo/controller/DemoController.java
@@ -1,5 +1,8 @@
 package org.tis.toolsfortest.usestarter.persistence.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import javax.annotation.Resource;
  *
  * @author Shiyunlai
  */
+@Api(tags = "Demo管理")
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
@@ -26,38 +30,40 @@ public class DemoController {
     @Resource
     private IDemoService demoService;
 
+    @ApiOperation("hello演示接口")
     @PostMapping("/hello")
-    public Object hello(@RequestBody Demo demo) {
+    public Object hello(@ApiParam("json形式的Demo对象信息") @RequestBody Demo demo) {
 
         logger.info(demo.toString());
         return demo;
     }
 
+    @ApiOperation("列出Demo树节点")
     @GetMapping("/tree")
     public Object tree() {
         return demoService.tree();
     }
 
+    @ApiOperation("列出（历史）Demo树节点")
     @GetMapping("/treehis")
     public Object treeHis() {
         return demoService.treeHis();
     }
 
+    @ApiOperation("增加Demo树节点")
     @PostMapping("")
     public void addDemoTree(@RequestBody Demo demo) {
         // 新增记录（指定数据源）
         demoService.add(demo);
     }
 
-    /**
-     * 记录历史记录
-     * @param demo
-     */
+    @ApiOperation("增加（历史）Demo树节点")
     @PostMapping("/his")
     public void addDemoTreeHis(@RequestBody Demo demo){
         demoService.addHis(demo);
     }
 
+    @ApiOperation("修改Demo节点")
     @PutMapping("")
     public void updateDemo( @RequestBody Demo demo){
         demoService.updateById(demo);
