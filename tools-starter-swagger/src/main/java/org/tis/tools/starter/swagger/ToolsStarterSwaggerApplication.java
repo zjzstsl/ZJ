52e90b95a93502a2408deaaefa5025dce7231437
shiyunlai <shiyunlai@gmail.com>
2018-03-13 15:44:18 +0800

    新增tools-starter-swagger项目
    提供tools-swagger-spring-boot-starter

---
+++ tools-starter-swagger/src/main/java/org/tis/tools/starter/swagger/ToolsStarterSwaggerApplication.java
@@ -0,0 +1,12 @@
+package org.tis.tools.starter.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsStarterSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsStarterSwaggerApplication.class, args);
+	}
+}
