2fd4f10b8e450edaf08b6e971c1b3d06c7a71465
shiyunlai <shiyunlai@gmail.com>
2018-03-15 10:41:27 +0800

    完成跨域能力集成

---
+++ tools-starter-cors/src/main/java/org/tis/tools/starter/cors/CorsWebMvcConfigurer.java
@@ -0,0 +1,29 @@
+package org.tis.tools.starter.cors;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 设置跨域配置
+ * @author zhaochunhai
+ * @author Shiyunlai
+ * @since 2018-03-15
+ */
+public class CorsWebMvcConfigurer implements WebMvcConfigurer{
+
+    CorsConfigProperties corsConfigProperties;
+
+    public CorsWebMvcConfigurer(CorsConfigProperties corsConfigProperties) {
+        this.corsConfigProperties = corsConfigProperties;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(corsConfigProperties.getMappingPath())
+                .allowedOrigins(corsConfigProperties.allowedOrigins)
+                .allowCredentials(corsConfigProperties.isAllowCredentials())
+                .allowedHeaders(corsConfigProperties.getAllowedHeaders())
+                .allowedMethods(corsConfigProperties.getAllowedMethods())
+                .maxAge(corsConfigProperties.getMaxAge());
+    }
+}
--- tools-starter-cors/src/main/java/org/tis/tools/starter/cors/EnableServiceCORS.java
+++ tools-starter-cors/src/main/java/org/tis/tools/starter/cors/EnableServiceCORS.java
@@ -4,10 +4,21 @@ import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
+/**
+ * <pre>
+ * 允许服务开启跨域访问
+ * 在Spring Boot启动类上注解 @EnableServiceCORS
+ * 跨域知识： http://www.ruanyifeng.com/blog/2016/04/cors.html
+ * 实现参考： https://www.jianshu.com/p/f2060a6d6e3b
+ * </pre>
+ *
+ * @author Shiyunlai
+ * @since 2018-03-15
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-@Import({CORSWebAppAutoConfiguration.class})
+@Import({CorsWebAppAutoConfiguration.class})
 public @interface EnableServiceCORS {
 }
