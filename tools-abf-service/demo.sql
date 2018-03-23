630b4c0af55742101426b703cc2daea7558b5c5d
shiyunlai <shiyunlai@gmail.com>
2018-03-23 11:02:00 +0800

    增加demo.sql

---
+++ tools-abf-service/demo.sql
@@ -0,0 +1,17 @@
+CREATE TABLE scaffold_demo
+(
+    guid varchar(128),
+    dName varchar(64),
+    dAge int(11),
+    dBirthday datetime,
+    dSex int(11)
+);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('123123', '123', 34, '2018-03-07 13:16:59', null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('232323', '321', null, '2018-03-08 13:17:09', null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('787878', '321', null, '2018-03-08 13:17:09', null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('1111', '1111', 111, '2018-03-12 08:47:22', null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('his1', null, null, null, null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('his2', null, null, null, null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('his33', null, null, null, null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('his44', null, null, null, null);
+INSERT INTO scaffold_demo (guid, dName, dAge, dBirthday, dSex) VALUES ('his5555', null, null, null, null);
