9064378c8757a72eb6b94d0088360585cedc9a3f
shiyunlai <shiyunlai@gmail.com>
2018-04-16 09:50:36 +0800

    运行asf

---
+++ tools-asf-service/src/main/java/org/tis/tools/asf/ToolsAsfServiceApplication.java
@@ -0,0 +1,26 @@
+package org.tis.tools.asf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <pre>
+ * Tools ASF Micro-Service 启动入口.
+ * 基于Spring Boot,以jar方式启动
+ * </pre>
+ *
+ * @author Shiyunlai
+ * @since 2018-03-02
+ */
+@SpringBootApplication
+public class ToolsAsfServiceApplication {
+
+    private final static Logger logger = LoggerFactory.getLogger(ToolsAsfServiceApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsAsfServiceApplication.class, args);
+        logger.info("Tools ASF Service Start Success!");
+    }
+}
