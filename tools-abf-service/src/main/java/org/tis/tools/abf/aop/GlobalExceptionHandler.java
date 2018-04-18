fa3e449eaeec8c08e760d83d3109670217598dc6
iiRGB <179494015@qq.com>
2018-04-18 15:26:18 +0800

    结构整理

--- tools-abf-service/src/main/java/org/tis/tools/abf/aop/GlobalExceptionHandler.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/aop/GlobalExceptionHandler.java
@@ -1,22 +1,8 @@
 package org.tis.tools.abf.aop;
 
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageConversionException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.tis.tools.core.aop.BaseControllerExceptionHandler;
-import org.tis.tools.core.exception.ToolsRuntimeException;
-import org.tis.tools.core.exception.WebAppException;
-import org.tis.tools.core.web.vo.ResultVO;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.Set;
 
 /**
  * describe: 统一异常处理
